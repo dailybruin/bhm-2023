8f2c973d5f81e7cbdce581dbfdd1ef4e814356c0
Nicole Ju <nicoleju@g.ucla.edu>
2023-02-16 16:53:30 -0800

    added the last section (section 4)

--- src/components/StoriesGrid.js
+++ src/components/StoriesGrid.js
@@ -14,17 +14,6 @@ const Section1 = styled.div`
     grid-template-columns: .4fr .6fr;
     column-gap: 1em
 `
-const Section3 = styled.div`
-    position: relative;
-    max-width: 95vw;
-    margin: auto;
-    display: grid;
-    grid-template-areas:
-    "card_two card_one"
-    "quote card_one";
-    grid-template-columns: .4fr .6fr;
-    column-gap: 1em;
-`
 const Section2 = styled.div`
     position: relative;
     max-width: 95vw;
@@ -40,19 +29,45 @@ const Section2 = styled.div`
     column-gap: 1em;
     row-gap: 1em;
 `
-const Container1 = styled.div`
+const Section3 = styled.div`
+    position: relative;
+    max-width: 95vw;
+    margin: auto;
+    display: grid;
+    grid-template-areas:
+    "card_two card_one"
+    "quote card_one";
+    grid-template-columns: .4fr .6fr;
+    column-gap: 1em;
+`
+const Section4 = styled.div`
+    position: relative;
+    max-width: 95vw;
+    margin: auto;
+    display: grid;
+    grid-template-areas:
+    "card_one card_two card_two"
+    "card_one card_two card_two"
+    "card_one block card_1"
+    "card_2 card_2 card_1"
+    "card_2 card_2 card_1";
+    grid-template-columns: 2fr 1fr 2fr;
+    column-gap: 1em;
+    row-gap: 1em;
+`
+const ContainerOne = styled.div`
     grid-area: card_one;
     position: relative;
 `
-const Container1_ = styled.div`
+const Container1 = styled.div`
     grid-area: card_1
     position: relative;
 `
-const Container2 = styled.div`
+const ContainerTwo = styled.div`
     grid-area: card_two;
     position: relative;
 `
-const Container2_ = styled.div`
+const Container2 = styled.div`
     grid-area: card_2;
     position: relative;
 `
@@ -65,10 +80,15 @@ const PullQuote = styled.div`
     text-align: left;
     font-family: "Smooch Sans", sans-serif;
 `
- const Block = styled.div`
+ const RedBlock = styled.div`
     grid-area: block;
     position: relative;
     background-color: #D3493D;
+ `
+ const GreenBlock = styled.div`
+    grid-area: block;
+    position: relative;
+    background-color: #387854;
  `
 
 export default function StoriesGrid(props){
@@ -90,7 +110,7 @@ export default function StoriesGrid(props){
                     }
                     else if(item.style == 'one'){
                         return(
-                            <Container1>
+                            <ContainerOne>
                                 <ArticleCard 
                                 article_title = {item.article_title}
                                 article_image = {item.article_image} 
@@ -100,12 +120,12 @@ export default function StoriesGrid(props){
                                 float = {item.float}
                                 color = {item.color}
                                 />
-                            </Container1>
+                            </ContainerOne>
                         )
                     }
                     else if(item.style == 'two'){
                         return(
-                            <Container2>
+                            <ContainerTwo>
                                 <ArticleCard 
                                 article_title = {item.article_title}
                                 article_image = {item.article_image} 
@@ -115,54 +135,58 @@ export default function StoriesGrid(props){
                                 float = {item.float}
                                 color = {item.color}
                                 />
-                            </Container2>
+                            </ContainerTwo>
                         )
                     }
                 })
             : null}
         </Section1>
         <Section2>
-            <Container2>
+            <ContainerTwo>
                 <ArticleCard 
                 article_title = {props.articles[3].article_title}
                 article_image = {props.articles[3].article_image}
+                article_url = {props.articles[3].article_url}
                 article_byline = {props.articles[3].article_byline}
                 style = {props.articles[3].style}
                 float = {props.articles[3].float}
                 color = {props.articles[3].color}
                 />
-            </Container2>
-            <Container1>
+            </ContainerTwo>
+            <ContainerOne>
             <ArticleCard 
                 article_title = {props.articles[4].article_title}
                 article_image = {props.articles[4].article_image}
+                article_url = {props.articles[4].article_url}
                 article_byline = {props.articles[4].article_byline}
                 style = {props.articles[4].style}
                 float = {props.articles[4].float}
                 color = {props.articles[4].color}
                 />
-            </Container1>
-            <Container1_>
+            </ContainerOne>
+            <Container1>
             <ArticleCard 
                 article_title = {props.articles[5].article_title}
                 article_image = {props.articles[5].article_image}
+                article_url = {props.articles[5].article_url}
                 article_byline = {props.articles[5].article_byline}
                 style = {props.articles[5].style}
                 float = {props.articles[5].float}
                 color = {props.articles[5].color}
                 />
-            </Container1_>
-            <Block></Block>
-            <Container2_>
+            </Container1>
+            <RedBlock></RedBlock>
+            <Container2>
             <ArticleCard 
                 article_title = {props.articles[6].article_title}
                 article_image = {props.articles[6].article_image}
+                article_url = {props.articles[6].article_url}
                 article_byline = {props.articles[6].article_byline}
                 style = {props.articles[6].style}
                 float = {props.articles[6].float}
                 color = {props.articles[6].color}
                 />
-            </Container2_>  
+            </Container2>  
         </Section2>
         <Section3>
         {props && props.articles && articles1
@@ -176,7 +200,7 @@ export default function StoriesGrid(props){
                     }
                     else if(item.style == 'one'){
                         return(
-                            <Container1>
+                            <ContainerOne>
                                 <ArticleCard 
                                 article_title = {item.article_title}
                                 article_image = {item.article_image} 
@@ -186,12 +210,12 @@ export default function StoriesGrid(props){
                                 float = {item.float}
                                 color = {item.color}
                                 />
-                            </Container1>
+                            </ContainerOne>
                         )
                     }
                     else if(item.style == 'two'){
                         return(
-                            <Container2>
+                            <ContainerTwo>
                                 <ArticleCard 
                                 article_title = {item.article_title}
                                 article_image = {item.article_image} 
@@ -201,12 +225,61 @@ export default function StoriesGrid(props){
                                 float = {item.float}
                                 color = {item.color}
                                 />
-                            </Container2>
+                            </ContainerTwo>
                         )
                     }
                 })
             : null}
         </Section3>
+        <Section4>
+            <ContainerOne>
+                <ArticleCard 
+                article_title = {props.articles[10].article_title}
+                article_image = {props.articles[10].article_image}
+                article_url = {props.articles[10].article_url}
+                article_byline = {props.articles[10].article_byline}
+                style = {props.articles[10].style}
+                float = {props.articles[10].float}
+                color = {props.articles[10].color}
+                />
+            </ContainerOne>
+            <ContainerTwo>
+            <ArticleCard 
+                article_title = {props.articles[11].article_title}
+                article_image = {props.articles[11].article_image}
+                article_url = {props.articles[11].article_url}
+                article_byline = {props.articles[11].article_byline}
+                style = {props.articles[11].style}
+                float = {props.articles[11].float}
+                color = {props.articles[11].color}
+                />
+            </ContainerTwo>
+            <Container2>
+            <ArticleCard 
+                article_title = {props.articles[12].article_title}
+                article_image = {props.articles[12].article_image}
+                article_url = {props.articles[12].article_url}
+                article_byline = {props.articles[12].article_byline}
+                style = {props.articles[12].style}
+                float = {props.articles[12].float}
+                color = {props.articles[12].color}
+                />
+            </Container2>
+            <GreenBlock></GreenBlock>
+            <Container1>
+            <ArticleCard 
+                article_title = {props.articles[13].article_title}
+                article_image = {props.articles[13].article_image}
+                article_url = {props.articles[13].article_url}
+                article_byline = {props.articles[13].article_byline}
+                style = {props.articles[13].style}
+                float = {props.articles[13].float}
+                color = {props.articles[13].color}
+                />
+            </Container1>  
+
+        </Section4>
+
         </div>
     )
 
